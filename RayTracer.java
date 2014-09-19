eece89235de45095fdddf7d6d0883068a63c4492
KiraLd <lucas.mohimont@hotmail.fr>
2014-09-19 16:16:41 +0200

    Create RayTracer.java

---
+++ RayTracer.java
@@ -0,0 +1,12 @@
+package raytracer;
+
+
+public class RayTracer {
+    public static void main(String[] args) {
+
+    //appelle la fonction principal du moteur
+    Affichage f = new Affichage();
+        
+    }
+    
+}
