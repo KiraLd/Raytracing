d69dcb40f009882b5529d8a65eae3a1428453822
KiraLd <lucas.mohimont@hotmail.fr>
2014-09-27 13:13:17 +0200

    Correction/optimisation du calcul de l'intersection

--- Sphere.java
+++ Sphere.java
@@ -20,9 +20,9 @@ public class Sphere extends Objet{
         rayon = 1.0;
     }
     
-    public Sphere(Vect3d c, double coef, Point p, double r)
+    public Sphere(Vect3d c, double coef, Vect3d d, double coef_d,Vect3d s,double c_s, Point p, double r)
     {
-        super(c,coef,p);
+        super(c,coef,d,coef_d,s,c_s,p);
         rayon=r;
     }
     
@@ -37,46 +37,42 @@ public class Sphere extends Objet{
     {
         Vect3d v = r.getDirection();
         Point p = r.getOrigine();
-        double x0 = getOrigine().getX();
-        double y0 = getOrigine().getY();
-        double z0 = getOrigine().getZ();
         double vx = v.getX();
         double vy = v.getY();
         double vz = v.getZ();
         double xr = p.getX();
         double yr = p.getY();
         double zr = p.getZ();
-        double a = vx * vx + vy * vy + vz * vz;
-        double b = 2 * (vx * (xr - x0) + vy * (yr - y0) + vz * (zr - z0));
-        double c = (xr - x0) * (xr - x0) + (yr - y0) * (yr - y0) + (zr - z0) * (zr - z0) - rayon * rayon;
-        double d = b * b - 4 * a * c;
-        double alpha1;
-        double alpha2;
-        if(d > 0)
-        {
-            alpha1 = (-b + Math.sqrt(d)) / 2 * a;
-            alpha2 = (-b - Math.sqrt(d)) / 2 * a;
-            Point p1 = new Point(alpha1 * vx + xr , alpha1 * vy + yr , alpha1 * vz + zr);
-            Point p2 = new Point(alpha2 * vx + xr , alpha2 * vy + yr , alpha2 * vz + zr);
-            if(p1.distance(r.getOrigine()) == rayon)
-            {
-                return p1;
-            }
-            else
-            {
-                return p2;
-            }
-        }
-        if(d == 0)
-        {
-            alpha1 = -b / 2 * a;
-            return new Point(alpha1 * vx + xr , alpha1 * vy + yr , alpha1 * vz + zr);
-        }
-        else
+        Vect3d oc = new Vect3d(r.getOrigine(),this.getOrigine());
+        double scalaire = oc.produit_scalaire(r.getDirection());
+        if(scalaire<0)
         {
             return new Point(Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY);
         }
-        
+        else
+        {
+            double ch = oc.module()*oc.module() - scalaire*scalaire;
+            if(ch>rayon*rayon)
+            {
+                return new Point(Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY,Double.POSITIVE_INFINITY);
+            }
+            else
+            {
+                double d2 = rayon*rayon -ch;
+                double t1 = scalaire + Math.sqrt(d2);
+                double t2 = scalaire - Math.sqrt(d2);
+                Point p1 = new Point(t1 * vx + xr , t1 * vy + yr , t1 * vz + zr);
+                Point p2 = new Point(t2 * vx + xr , t2 * vy + yr , t2 * vz + zr);
+                if(t1<t2&&t1>0)
+                {
+                    return p1;
+                }
+                else
+                {
+                    return p2;
+                }
+            }
+        }
     }
     
 }
