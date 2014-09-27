9770204ef72c8090a1ecce3ee6d2cfacb251f6c9
KiraLd <lucas.mohimont@hotmail.fr>
2014-09-27 13:11:55 +0200

    Ajout coef couleur diffuse/spéculaire

--- Objet.java
+++ Objet.java
@@ -12,8 +12,12 @@ package raytracer;
 
 public abstract class Objet {
     
-    private Vect3d couleur;         //Représente une couleur rvb
-    private double coef_couleur;    //coefficient associer à la couleur
+    private Vect3d couleur_ambiante;         //Représente une couleur rvb
+    private double coef_couleur_ambiante;    //coefficient associer à la couleur
+    private Vect3d couleur_diffuse;
+    private double coef_couleur_diffuse;
+    private Vect3d couleur_speculaire;
+    private double coef_couleur_speculaire;
     private Point p;                //origine de l'objet(sauf pour les plans)
     
     
@@ -21,30 +25,59 @@ public abstract class Objet {
     
     public Objet()
     {
-        couleur = new Vect3d();
-        coef_couleur = 0.0;
+        couleur_ambiante = new Vect3d();
+        coef_couleur_ambiante = 0.0;
+        couleur_diffuse = new Vect3d();
+        coef_couleur_diffuse = 0.0;
+        couleur_speculaire = new Vect3d();
+        coef_couleur_speculaire = 0.0;
         p = new Point();
     }
     
-    public Objet(Vect3d c, double coef, Point p)
+    public Objet(Vect3d c, double coef, Vect3d d, double coef_d,Vect3d s,double coef_s,Point p)
     {
-        couleur = new Vect3d(c);
-        coef_couleur = coef;
+        couleur_ambiante = new Vect3d(c);
+        coef_couleur_ambiante = coef;
+        couleur_diffuse = new Vect3d(d);
+        coef_couleur_diffuse = coef_d;
+        couleur_speculaire = new Vect3d(s);
+        coef_couleur_speculaire = coef_s;
         this.p = new Point(p);
     }
     
     
     //getters
     
-    public Vect3d getCouleur()
+    public Vect3d getCouleurAmbiante()
     {
-        return couleur;
+        return couleur_ambiante;
     }
     
-    public double getCoefCouleur()
+    public double getCoefCouleurAmbiante()
     {
-        return coef_couleur;
+        return coef_couleur_ambiante;
     }
+    
+    public Vect3d getCouleurDiffuse()
+    {
+        return couleur_diffuse;
+    }
+    
+    public double getCoefCouleurDiffuse()
+    {
+        return coef_couleur_diffuse;
+    }
+    
+     public double getCoefCouleurSpeculaire()
+    {
+        return coef_couleur_speculaire;
+    }
+     
+    public Vect3d getCouleurSpeculaire()
+    {
+        return couleur_speculaire;
+    }
+    
     
     public Point getOrigine()
     {
@@ -55,5 +88,8 @@ public abstract class Objet {
     
     public abstract Point intersection(Rayon r);
 }
+
+
+
 
 
